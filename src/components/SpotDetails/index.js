e406c19c584c31a77757f7229c9f72d47d92a538
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2023-11-30 02:08:04 -0800

    completed template of spot details page and added some flex styling

--- src/components/SpotDetails/index.js
+++ src/components/SpotDetails/index.js
@@ -1,9 +1,53 @@
+import { useParams } from "react-router-dom";
+import "./SpotDetails.css";
+import data from "../../seeds/data.json";
+
 const SpotDetails = () => {
-    return (
-        <>
-            <h1>SpotDetails</h1>
-        </>
-    )
-}
+  const { spotId } = useParams();
+  const spot = data[spotId];
+
+  return (
+    <div className="spot-details-container">
+        <div className="spot-details">
+
+      <h1>{spot.name}</h1>
+      <p>
+        {spot.city}, {spot.state}, {spot.country}
+      </p>
+      <div className="images-grid">
+        <img className="large-image" src={spot.image} alt="spot-image" />
+        <div className="small-images-container">
+          <div>
+            <img className="small-images" src={spot.image} alt="spot-image" />
+            <img className="small-images" src={spot.image} alt="spot-image" />
+          </div>
+          <div>
+            <img className="small-images" src={spot.image} alt="spot-image" />
+            <img className="small-images" src={spot.image} alt="spot-image" />
+          </div>
+        </div>
+      </div>
+      <div className="details">
+        <div>
+          <span>
+            Hosted by {spot.owner.firstName} {spot.owner.lastName}
+          </span>
+          <p>{spot.description}</p>
+        </div>
+        <div className="box">
+          <span className="box-info">
+            <p>${spot.price} night</p>
+            <p>
+              <i class="fa-solid fa-star"></i>
+              {spot.numStars}
+            </p>
+          </span>
+          <button>Reserve</button>
+        </div>
+      </div>
+        </div>
+    </div>
+  );
+};
 
 export default SpotDetails;
--- src/components/Spots/index.js
+++ src/components/Spots/index.js
@@ -1,16 +1,16 @@
-import SpotTile from '../SpotTile';
-import './Spots.css'
-import data from './seeders.json';
+import SpotTile from "../SpotTile";
+import "./Spots.css";
+import data from "../../seeds/data.json";
 
 const Spots = () => {
-    const spots = Object.values(data);
-    return (
-        <div className="spots">
-            {spots.map((spot, idx) => (
-                <SpotTile spot={spot} id={idx+1} key={idx+1}/>
-            ))}
-        </div>
-    )
-}
+  const spots = Object.values(data);
+  return (
+    <div className="spots">
+      {spots.map((spot, idx) => (
+        <SpotTile spot={spot} id={idx + 1} key={idx + 1} />
+      ))}
+    </div>
+  );
+};
 
 export default Spots;
