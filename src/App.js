4267cff70fa3135681e2e402ddd4cb15ed02663d
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2023-11-29 23:32:58 -0800

    built out template of navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+
 const App = () => {
   return (
     <>
+      <Navigation />
       <h1>Hello from App</h1>
     </>
   );
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import './Navigation.css'
+
+const Navigation = () => {
+    return (
+        <nav>
+            <div id="nav-left">airbnb</div>
+            <div id="nav-right">dropdown</div>
+        </nav>
+    )
+}
+
+export default Navigation;
