b57d9acfe326420dfa11ba6faec7a61aa7968542
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2023-11-30 20:28:27 -0800

    added more styling to spots details page to match the wireframes

--- src/components/SpotDetails/index.js
+++ src/components/SpotDetails/index.js
@@ -8,44 +8,43 @@ const SpotDetails = () => {
 
   return (
     <div className="spot-details-container">
-        <div className="spot-details">
-
-      <h1>{spot.name}</h1>
-      <p>
-        {spot.city}, {spot.state}, {spot.country}
-      </p>
-      <div className="images-grid">
-        <img className="large-image" src={spot.image} alt="spot-image" />
-        <div className="small-images-container">
-          <div>
-            <img className="small-images" src={spot.image} alt="spot-image" />
-            <img className="small-images" src={spot.image} alt="spot-image" />
+      <div className="spot-details">
+        <h1>{spot.name}</h1>
+        <p>
+          {spot.city}, {spot.state}, {spot.country}
+        </p>
+        <div className="images-grid">
+          <img className="large-image" src={spot.image} alt="spot-image" />
+          <div className="small-images-container">
+            <div className="image-columns">
+              <img className="small-images" src={spot.image} alt="spot-image" />
+              <img className="small-images bottom-images" src={spot.image} alt="spot-image" />
+            </div>
+            <div class="image-columns">
+              <img className="small-images" src={spot.image} alt="spot-image" />
+              <img className="small-images bottom-images" src={spot.image} alt="spot-image" />
+            </div>
           </div>
-          <div>
-            <img className="small-images" src={spot.image} alt="spot-image" />
-            <img className="small-images" src={spot.image} alt="spot-image" />
+        </div>
+        <div className="details">
+          <div className="owner-and-description">
+            <span>
+              <h2>Hosted by {spot.owner.firstName} {spot.owner.lastName}</h2>
+            </span>
+            <p>{spot.description}</p>
+          </div>
+          <div className="box">
+            <span className="box-info">
+              <p><b style={{fontSize: "20px"}}>${spot.price}</b> night</p>
+              <p>
+                <i class="fa-solid fa-star"></i>
+                {spot.numStars}
+              </p>
+            </span>
+            <button onClick={() => window.alert("Feature coming soon...")}>Reserve</button>
           </div>
         </div>
       </div>
-      <div className="details">
-        <div>
-          <span>
-            Hosted by {spot.owner.firstName} {spot.owner.lastName}
-          </span>
-          <p>{spot.description}</p>
-        </div>
-        <div className="box">
-          <span className="box-info">
-            <p>${spot.price} night</p>
-            <p>
-              <i class="fa-solid fa-star"></i>
-              {spot.numStars}
-            </p>
-          </span>
-          <button>Reserve</button>
-        </div>
-      </div>
-        </div>
     </div>
   );
 };
